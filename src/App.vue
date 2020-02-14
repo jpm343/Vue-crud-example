64b87a814326f7e35d2f852e2b33e5d85f8cc1f3
juan.martinez <juan.martinez@solem.cl>
2020-02-14 12:19:37 -0300

    ADD: users card

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,32 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        text
+        rounded
+        class="my-2"
+        :to="{ name: link.name }"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-content>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title class="pb-0">
-          <h1>Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
-        <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="{ name: link.name }"
+          >{{ link.label }}</v-btn
+        >
         <v-flex primary lighten-2 py-4 text-center white--text xs12>
           {{ new Date().getFullYear() }} —
           <strong>Vuetify Dashboard</strong>
@@ -50,8 +46,16 @@ export default {
 
   data() {
     return {
-      showPassword: false,
-      links: ["Home", "Login"]
+      links: [
+        {
+          label: "Home",
+          name: "home"
+        },
+        {
+          label: "Login",
+          name: "login"
+        }
+      ]
     };
   }
 };
